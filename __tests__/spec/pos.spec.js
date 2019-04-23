690aaccfe85d996a7df020749afbb36460789e59
pistis <vamalboro@gmail.com>
2019-04-24 00:52:09 +0900

    refactor part-of-speech module

--- __tests__/spec/pos.spec.js
+++ __tests__/spec/pos.spec.js
@@ -1,31 +1,27 @@
-const { lookup } = require('../../src/pos')
-const path = require('path')
+const { getPOS } = require('../../src/pos')
 
-describe('lookup test', () => {
-  test('one word', async () => {
+describe('getPOS test', () => {
+  test('noun word', async () => {
     expect.assertions(1)
-    const data = await lookup(['apple'])
-    expect(data[0].part).toEqual('noun')
+    const pos = await getPOS(['naming', 'analysis'])
+    expect(pos.nouns.length).toBe(2)
   })
 
-  test('multiple words', async () => {
-    expect.assertions(10)
-    const data = await lookup([
-      'extract',
-      'split',
-      'pos',
-      'extractor',
-      'splitter',
-    ])
-    expect(data[0].word).toEqual('extract')
-    expect(data[0].part).toEqual('noun')
-    expect(data[1].word).toEqual('split')
-    expect(data[1].part).toEqual('adj')
-    expect(data[2].word).toEqual('pos')
-    expect(data[2].part).toEqual('')
-    expect(data[3].word).toEqual('extractor')
-    expect(data[3].part).toEqual('noun')
-    expect(data[4].word).toEqual('splitter')
-    expect(data[4].part).toEqual('noun')
+  test('adjectives word', async () => {
+    expect.assertions(1)
+    const pos = await getPOS(['beautiful', 'naming'])
+    expect(pos.adjectives.length).toBe(2)
+  })
+
+  test('adverbs word', async () => {
+    expect.assertions(1)
+    const pos = await getPOS(['definitely'])
+    expect(pos.adverbs.length).toBe(1)
+  })
+
+  test('verbs word', async () => {
+    expect.assertions(1)
+    const pos = await getPOS(['make'])
+    expect(pos.verbs.length).toBe(1)
   })
 })
--- src/pos.js
+++ src/pos.js
@@ -2,25 +2,10 @@
 const WordPOS = require('wordpos')
 const wordpos = new WordPOS()
 
-const _lookup = async function(word) {
-  return new Promise((resolve, reject) => {
-    wordpos.lookup(word, function(pos, word) {
-      // If there are various parts of speech, only the first lookup result is used.
-      const lexName = pos.length > 0 ? pos[0].lexName : ''
-      const part = lexName.split('.')
-      resolve({ word, part: part[0] })
-    })
-  })
-}
-
-const lookup = async function(words) {
-  try {
-    return await Promise.all(words.map((word) => _lookup(word)))
-  } catch (e) {
-    throw e
-  }
+const getPOS = async function(words) {
+  return await wordpos.getPOS(words)
 }
 
 module.exports = {
-  lookup,
+  getPOS,
 }
