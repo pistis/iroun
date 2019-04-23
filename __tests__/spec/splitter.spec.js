ad786a491e763af5578c88132ad0e80babdfcc08
pistis <vamalboro@gmail.com>
2019-04-23 19:10:30 +0900

    implement splitCamelCase, splitPascalCase, splitSnakeCase and refactoring split method

--- __tests__/spec/splitter.spec.js
+++ __tests__/spec/splitter.spec.js
@@ -1,54 +1,201 @@
-const { split } = require('../../src/splitter.js')
+const {
+  split,
+  splitCamelCase,
+  splitSnakeCase,
+  splitPascalCase,
+} = require('../../src/splitter.js')
 
 describe('split test', () => {
-  describe('camel case', () => {
-    test('2 words', () => {
-      const wordList = split('camelCase')
-      expect(wordList.length).toBe(2)
-      expect(wordList).toContain('camel')
-      expect(wordList).toContain('Case')
+  // complex cases
+  describe('split test', () => {
+    test('camel case', () => {
+      const words = split('camelCase')
+      expect(words.length).toBe(2)
+      expect(words).toContain('camel')
+      expect(words).toContain('Case')
     })
-
-    test('3 words', () => {
-      const wordList = split('camelCaseTest')
-      expect(wordList.length).toBe(3)
-      expect(wordList).toContain('camel')
-      expect(wordList).toContain('Case')
-      expect(wordList).toContain('Test')
+    test('pascal case', () => {
+      const words = split('PascalCase')
+      expect(words.length).toBe(2)
+      expect(words).toContain('Pascal')
+      expect(words).toContain('Case')
     })
-
-    test('4 words', () => {
-      const wordList = split('camelCaseTestCases')
-      expect(wordList.length).toBe(4)
-      expect(wordList).toContain('camel')
-      expect(wordList).toContain('Case')
-      expect(wordList).toContain('Test')
-      expect(wordList).toContain('Cases')
+    test('snake case', () => {
+      const words = split('snake_case')
+      expect(words.length).toBe(2)
+      expect(words).toContain('snake')
+      expect(words).toContain('case')
     })
   })
-  describe('pascal case', () => {
+  // camelCase
+  describe('splitCamelCase test', () => {
     test('2 words', () => {
-      const wordList = split('PascalCase')
-      expect(wordList.length).toBe(2)
-      expect(wordList).toContain('Pascal')
-      expect(wordList).toContain('Case')
+      const words = splitCamelCase('camelCase')
+      expect(words.length).toBe(2)
+      expect(words).toContain('camel')
+      expect(words).toContain('Case')
     })
 
     test('3 words', () => {
-      const wordList = split('PascalCaseTest')
-      expect(wordList.length).toBe(3)
-      expect(wordList).toContain('Pascal')
-      expect(wordList).toContain('Case')
-      expect(wordList).toContain('Test')
+      const words = splitCamelCase('camelCaseTest')
+      expect(words.length).toBe(3)
+      expect(words).toContain('camel')
+      expect(words).toContain('Case')
+      expect(words).toContain('Test')
     })
 
     test('4 words', () => {
-      const wordList = split('PascalCaseTestCases')
-      expect(wordList.length).toBe(4)
-      expect(wordList).toContain('Pascal')
-      expect(wordList).toContain('Case')
-      expect(wordList).toContain('Test')
-      expect(wordList).toContain('Cases')
+      const words = splitCamelCase('camelCaseTestCases')
+      expect(words.length).toBe(4)
+      expect(words).toContain('camel')
+      expect(words).toContain('Case')
+      expect(words).toContain('Test')
+      expect(words).toContain('Cases')
+    })
+
+    test('5 words and two over Upper case', () => {
+      const words = splitCamelCase('camelCaseTestCasesAABBCC')
+      expect(words.length).toBe(5)
+      expect(words).toContain('camel')
+      expect(words).toContain('Case')
+      expect(words).toContain('Test')
+      expect(words).toContain('Cases')
+      expect(words).toContain('AABBCC')
+    })
+    test('3 words and two over Upper case', () => {
+      const words = splitCamelCase('camelAACase')
+      expect(words.length).toBe(3)
+      expect(words).toContain('camel')
+      expect(words).toContain('AA')
+      expect(words).toContain('Case')
+    })
+    test('complex case', () => {
+      // can't split AREU -> ARE and U
+      const words = splitCamelCase('thisIsAPainWhatAREUDoingNow')
+      expect(words.length).toBe(8)
+      expect(words).toContain('this')
+      expect(words).toContain('Is')
+      expect(words).toContain('A')
+      expect(words).toContain('Pain')
+      expect(words).toContain('What')
+      expect(words).toContain('AREU')
+      expect(words).toContain('Doing')
+      expect(words).toContain('Now')
+    })
+  })
+
+  // snake_case
+  describe('splitSnakeCase test', () => {
+    test('2 words', () => {
+      const words = splitSnakeCase('snake_case')
+      expect(words.length).toBe(2)
+      expect(words).toContain('snake')
+      expect(words).toContain('case')
+    })
+
+    test('3 words', () => {
+      const words = splitSnakeCase('snake_case_test')
+      expect(words.length).toBe(3)
+      expect(words).toContain('snake')
+      expect(words).toContain('case')
+      expect(words).toContain('test')
+    })
+
+    test('4 words', () => {
+      const words = splitSnakeCase('snake_test_case_cases')
+      expect(words.length).toBe(4)
+      expect(words).toContain('snake')
+      expect(words).toContain('test')
+      expect(words).toContain('case')
+      expect(words).toContain('cases')
+    })
+
+    test('5 words with Upper case', () => {
+      const words = splitSnakeCase('snake_test_cases_UPPER_CASE')
+      expect(words.length).toBe(5)
+      expect(words).toContain('snake')
+      expect(words).toContain('test')
+      expect(words).toContain('cases')
+      expect(words).toContain('UPPER')
+      expect(words).toContain('CASE')
+    })
+    test('3 words with Upper case', () => {
+      const words = splitSnakeCase('snake_AA_case')
+      expect(words.length).toBe(3)
+      expect(words).toContain('snake')
+      expect(words).toContain('AA')
+      expect(words).toContain('case')
+    })
+    test('complex case', () => {
+      // can't split AREU -> ARE and U
+      const words = splitSnakeCase('this_is_a_pain_What_Are_U_Doing_Now')
+      expect(words.length).toBe(9)
+      expect(words).toContain('this')
+      expect(words).toContain('is')
+      expect(words).toContain('a')
+      expect(words).toContain('pain')
+      expect(words).toContain('What')
+      expect(words).toContain('Are')
+      expect(words).toContain('U')
+      expect(words).toContain('Doing')
+      expect(words).toContain('Now')
+    })
+  })
+
+  // PascalCase
+  describe('splitCamelCase test', () => {
+    test('2 words', () => {
+      const words = splitPascalCase('CamelCase')
+      expect(words.length).toBe(2)
+      expect(words).toContain('Camel')
+      expect(words).toContain('Case')
+    })
+
+    test('3 words', () => {
+      const words = splitPascalCase('CamelCaseTest')
+      expect(words.length).toBe(3)
+      expect(words).toContain('Camel')
+      expect(words).toContain('Case')
+      expect(words).toContain('Test')
+    })
+
+    test('4 words', () => {
+      const words = splitPascalCase('CamelCaseTestCases')
+      expect(words.length).toBe(4)
+      expect(words).toContain('Camel')
+      expect(words).toContain('Case')
+      expect(words).toContain('Test')
+      expect(words).toContain('Cases')
+    })
+
+    test('5 words and two over Upper case', () => {
+      const words = splitPascalCase('CamelCaseTestCasesAABBCC')
+      expect(words.length).toBe(5)
+      expect(words).toContain('Camel')
+      expect(words).toContain('Case')
+      expect(words).toContain('Test')
+      expect(words).toContain('Cases')
+      expect(words).toContain('AABBCC')
+    })
+    test('3 words and two over Upper case', () => {
+      const words = splitPascalCase('CamelAACase')
+      expect(words.length).toBe(3)
+      expect(words).toContain('Camel')
+      expect(words).toContain('AA')
+      expect(words).toContain('Case')
+    })
+    test('complex case', () => {
+      // can't split AREU -> ARE and U
+      const words = splitPascalCase('ThisIsAPainWhatAREUDoingNow')
+      expect(words.length).toBe(8)
+      expect(words).toContain('This')
+      expect(words).toContain('Is')
+      expect(words).toContain('A')
+      expect(words).toContain('Pain')
+      expect(words).toContain('What')
+      expect(words).toContain('AREU')
+      expect(words).toContain('Doing')
+      expect(words).toContain('Now')
     })
   })
 })
--- src/splitter.js
+++ src/splitter.js
@@ -1,41 +1,69 @@
-/**
- * Split the name by word.
- * PascalCase
- * camelCase
- * snake_case
- * time complexity : O(n)
- */
-const split = function(name) {
-  let len = name.length
+const makeWordList = function(tokens) {
+  const words = []
   let word = ''
-  let list = []
-  let combine = false
+  tokens.forEach((token) => {
+    let code = token.charCodeAt(0)
 
-  for (let i = 0; i < len; i++) {
-    let char = name[i]
-    let code = char.charCodeAt(0)
-
-    if (code >= 97 && code <= 122) {
-      combine = true
-    } else if (code >= 65 && code <= 90) {
-      combine = false
-    }
-
-    if (combine) {
-      word += char
+    if (code >= 65 && code <= 90 && token.length === 1) {
+      word += token
     } else {
+      words.push(token)
       if (word.length > 0) {
-        list.push(word)
+        words.push(word)
+        word = ''
       }
-      word = char
     }
-  }
+  })
+
   if (word.length > 0) {
-    list.push(word)
+    words.push(word)
+    word = ''
   }
-  return list
+
+  return words
+}
+const splitCamelCase = function(name) {
+  const tokens = name
+    .replace(/([A-Z])/g, ' $1')
+    .trim()
+    .split(' ')
+
+  return makeWordList(tokens)
+}
+
+const splitPascalCase = function(name) {
+  const tokens = name
+    .replace(/([A-Z])/g, ' $1')
+    .trim()
+    .split(' ')
+
+  return makeWordList(tokens)
+}
+
+const splitSnakeCase = function(name) {
+  const tokens = name
+    .replace(/([_])/g, ' ')
+    .trim()
+    .split(' ')
+
+  return makeWordList(tokens)
+}
+
+const split = function(name) {
+  if (!name) return []
+  if (name.indexOf('_') !== -1) {
+    return splitSnakeCase(name)
+  } else if (/^[a-z]/.test(name)) {
+    return splitCamelCase(name)
+  } else if (/^[A-Z]/.test(name)) {
+    return splitPascalCase(name)
+  }
+  return []
 }
 
 module.exports = {
   split,
+  splitCamelCase,
+  splitSnakeCase,
+  splitPascalCase,
 }
