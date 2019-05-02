ecbc1d91dbe27886673249ef01acc4d04d6c8f09
pistis <vamalboro@gmail.com>
2019-05-02 13:14:30 +0900

    chore: change absolute path to relative path

--- scripts/extract-word-github.js
+++ scripts/extract-word-github.js
@@ -3,54 +3,54 @@ const { makeWordFile } = require('./extract-word')
 const repositories = [
   {
     path:
-      '/Users/user/Desktop/dev/private/iroun/.resources/react/packages/react/src',
-    output: '/Users/user/Desktop/dev/private/iroun/database/words/react.txt',
+      '../.resources/react/packages/react/src',
+    output: '../database/words/react.txt',
   },
   {
     path:
-      '/Users/user/Desktop/dev/private/iroun/.resources/react/packages/react-dom/src',
+      '../.resources/react/packages/react-dom/src',
     output:
-      '/Users/user/Desktop/dev/private/iroun/database/words/react-dom.txt',
+      '../database/words/react-dom.txt',
   },
   {
     path:
-      '/Users/user/Desktop/dev/private/iroun/.resources/react/packages/react-events/src',
+      '../.resources/react/packages/react-events/src',
     output:
-      '/Users/user/Desktop/dev/private/iroun/database/words/react-events.txt',
+      '../database/words/react-events.txt',
   },
   {
-    path: '/Users/user/Desktop/dev/private/iroun/.resources/vue/src',
-    output: '/Users/user/Desktop/dev/private/iroun/database/words/vue.txt',
+    path: '../.resources/vue/src',
+    output: '../database/words/vue.txt',
   },
   {
-    path: '/Users/user/Desktop/dev/private/iroun/.resources/svelte/src',
-    output: '/Users/user/Desktop/dev/private/iroun/database/words/svelte.txt',
+    path: '../.resources/svelte/src',
+    output: '../database/words/svelte.txt',
   },
   {
-    path: '/Users/user/Desktop/dev/private/iroun/.resources/riot/lib',
-    output: '/Users/user/Desktop/dev/private/iroun/database/words/riot.txt',
+    path: '../.resources/riot/lib',
+    output: '../database/words/riot.txt',
   },
   {
-    path: '/Users/user/Desktop/dev/private/iroun/.resources/polymer/lib',
-    output: '/Users/user/Desktop/dev/private/iroun/database/words/polymer.txt',
+    path: '../.resources/polymer/lib',
+    output: '../database/words/polymer.txt',
   },
   {
-    path: '/Users/user/Desktop/dev/private/iroun/.resources/nuclear-js/src',
+    path: '../.resources/nuclear-js/src',
     output:
-      '/Users/user/Desktop/dev/private/iroun/database/words/nuclear-js.txt',
+      '../database/words/nuclear-js.txt',
   },
   {
-    path: '/Users/user/Desktop/dev/private/iroun/.resources/matreshka/src',
+    path: '../.resources/matreshka/src',
     output:
-      '/Users/user/Desktop/dev/private/iroun/database/words/matreshka.txt',
+      '../database/words/matreshka.txt',
   },
   {
-    path: '/Users/user/Desktop/dev/private/iroun/.resources/marko/src',
-    output: '/Users/user/Desktop/dev/private/iroun/database/words/marko.txt',
+    path: '../.resources/marko/src',
+    output: '../database/words/marko.txt',
   },
   {
-    path: '/Users/user/Desktop/dev/private/iroun/.resources/knockout/src',
-    output: '/Users/user/Desktop/dev/private/iroun/database/words/knockout.txt',
+    path: '../.resources/knockout/src',
+    output: '../database/words/knockout.txt',
   },
 ]
 
