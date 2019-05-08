98198d89c7987db54a92ae221435fa793c99970d
pistis <vamalboro@gmail.com>
2019-05-09 08:19:16 +0900

    [#1] Creating the structure of the cli index file

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,33 @@
 #!/usr/bin/env node
+const program = require('commander')
+const ora = require('ora')
 
-console.log('welcome iroun cli')
+program.version('0.2.0', '-v, --version')
+program.option('-d, --directory-path <type>', 'set javascript source code absolute directory path.')
+program.on('--help', function() {
+  console.log('')
+  console.log('Examples:')
+  console.log('  $ iroun -d "absolute directory path."')
+})
+
+program.parse(process.argv)
+
+if (!program.directoryPath) {
+  console.log('set the source code directory.')
+  console.log('')
+  console.log('Examples:')
+  console.log('  $ iroun -d "absolute directory path."')
+  process.exit()
+}
+
+const spinner = ora({
+  text: 'Analyzing...',
+  spinner: 'arrow3',
+}).start()
+setTimeout(function() {
+  if (program.directoryPath) {
+    // if finish analsys
+    spinner.stop()
+    console.log('Topic Words Dataset is ...')
+  }
+}, 3000)
